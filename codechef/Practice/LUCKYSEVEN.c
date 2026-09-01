0b15b5b94a6362c85b5056f2b0d854c4bcfea13c
aamir27bit <aamirbharmal70@gmail.com>
2026-09-01 23:55:36 +0530

    Solved: LUCKYSEVEN (codechef)

---
+++ codechef/Practice/LUCKYSEVEN.c
@@ -0,0 +1,19 @@
+// Problem: LUCKYSEVEN
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/LUCKYSEVEN
+// Solved on: 2026-09-01T18:25:36.134Z
+
+#include <stdio.h>
+
+int main() {
+    char S[11];
+
+    scanf("%s", S);
+
+    printf("%c\n", S[6]);
+
+    return 0;
+}
+
