3dae3e7067023c2987867734c9a72e4c7a552ac5
aamir27bit <aamirbharmal70@gmail.com>
2026-09-01 20:54:39 +0530

    Solved: TIMELY (codechef)

---
+++ codechef/Practice/TIMELY.c
@@ -0,0 +1,30 @@
+// Problem: TIMELY
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TIMELY
+// Solved on: 2026-09-01T15:24:38.716Z
+
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d",&t); 
+   
+   for(int i=0;i<t;i++) {
+       int x;
+       
+       scanf("%d",&x);
+            
+            if(x>=30)
+            printf("YES\n");
+            
+            else
+            printf("NO\n");
+       
+   }
+ 
+        return 0;
+    
+}
+
