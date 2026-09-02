3f28d26ed6c1281428ab9cbd47851308991b6e13
aamir27bit <aamirbharmal70@gmail.com>
2026-09-02 23:35:17 +0530

    Solved: JASSIGNMENTS (codechef)

---
+++ codechef/Practice/JASSIGNMENTS.c
@@ -0,0 +1,28 @@
+// Problem: JASSIGNMENTS
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/JASSIGNMENTS
+// Solved on: 2026-09-02T18:05:17.134Z
+
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++) {
+    int x;
+   
+    scanf("%d",&x);
+
+    if(x<=7)
+    printf("YES\n");
+    else
+    printf("NO\n");
+    
+}
+
+    return 0;
+
+}
