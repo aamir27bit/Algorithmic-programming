43b8132dce9992b83470006c286d6438f7179335
aamir27bit <aamirbharmal70@gmail.com>
2026-09-02 22:40:28 +0530

    Solved: SUBSCRIBE_ (codechef)

---
+++ codechef/Practice/SUBSCRIBE_.c
@@ -0,0 +1,25 @@
+// Problem: SUBSCRIBE_
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/SUBSCRIBE_
+// Solved on: 2026-09-02T17:10:26.798Z
+
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++) {
+        int  x , y, z;
+
+        scanf("%d %d", &x, &y);
+
+        z = (x + 5) / 6;
+
+        printf("%d\n", z*y);
+    }
+
+    return 0;
+}
