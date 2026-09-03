5c4175d8657a1c611cbe23f5716b6db0770e1f10
aamir27bit <aamirbharmal70@gmail.com>
2026-09-03 15:36:16 +0530

    Solved: CREDCOINS (codechef)

---
+++ codechef/Practice/CREDCOINS.c
@@ -0,0 +1,22 @@
+// Problem: CREDCOINS
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/CREDCOINS
+// Solved on: 2026-09-03T10:06:15.876Z
+
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++) {
+        int X, Y;
+        scanf("%d %d", &X, &Y);
+
+        printf("%d\n", (X * Y) / 100);
+    }
+
+    return 0;
+}
