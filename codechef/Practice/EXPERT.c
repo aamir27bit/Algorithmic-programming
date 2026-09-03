8041ac8d7a5c06a3fa3b44149e7469cdd8752add
aamir27bit <aamirbharmal70@gmail.com>
2026-09-04 00:27:51 +0530

    Solved: EXPERT (codechef)

---
+++ codechef/Practice/EXPERT.c
@@ -0,0 +1,26 @@
+// Problem: EXPERT
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/EXPERT
+// Solved on: 2026-09-03T18:57:50.572Z
+
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++) {
+        int x, y;
+
+        scanf("%d %d", &x, &y);
+
+        if (y * 100 >= 50 * x)
+            printf("yes\n");
+        else
+            printf("no\n");
+    }
+
+    return 0;
+}
