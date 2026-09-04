053211be86ba92a6b90d4e2914cb1e463ae743f0
aamir27bit <aamirbharmal70@gmail.com>
2026-09-04 13:07:22 +0530

    Solved: HELIUM3 (codechef)

---
+++ codechef/Practice/HELIUM3.c
@@ -0,0 +1,29 @@
+// Problem: HELIUM3
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/HELIUM3
+// Solved on: 2026-09-04T07:37:21.919Z
+
+#include <stdio.h>
+
+int main() {
+
+    int t;
+    scanf("%d", &t);
+
+    for(int i = 0; i < t; i++) {
+
+        int a, b, x, y;
+        scanf("%d %d %d %d", &a, &b, &x, &y);
+
+        if(x * y >= a * b) {
+            printf("Yes\n");
+        }
+        else {
+            printf("No\n");
+        }
+    }
+
+    return 0;
+}
