b7a80d78bbeb0d44d9a417cd13f5ea509ec802aa
aamir27bit <aamirbharmal70@gmail.com>
2026-09-04 23:29:22 +0530

    Solved: TRAVELFAST (codechef)

---
+++ codechef/Practice/TRAVELFAST.c
@@ -0,0 +1,30 @@
+// Problem: TRAVELFAST
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/TRAVELFAST
+// Solved on: 2026-09-04T17:59:22.264Z
+
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++) {
+        int x, y;
+        scanf("%d %d", &x, &y);
+
+        if (x < y) 
+            printf("BIKE\n");
+        
+        else if (x > y) 
+            printf("CAR\n");
+        
+        else 
+            printf("SAME\n");
+        
+    }
+
+    return 0;
+}
