4f321f7147c06e58b91824ac1301201c3f1a39ab
aamir27bit <aamirbharmal70@gmail.com>
2026-09-04 13:09:52 +0530

    Solved: SUGARCANE (codechef)

---
+++ codechef/Practice/SUGARCANE.c
@@ -0,0 +1,27 @@
+// Problem: SUGARCANE
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/SUGARCANE
+// Solved on: 2026-09-04T07:39:52.008Z
+
+#include <stdio.h>
+
+int main() {
+
+    int t;
+    scanf("%d", &t);
+
+    for(int i = 0; i < t; i++) {
+
+        int x, z;
+        scanf("%d", &x);
+
+        z = x * 50;
+        z = z - (70 * z) / 100;
+
+        printf("%d\n", z);
+    }
+
+    return 0;
+}
