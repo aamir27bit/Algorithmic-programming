d14d7e59128cbd844d73745f965352560115f688
aamir27bit <aamirbharmal70@gmail.com>
2026-09-07 00:02:13 +0530

    Solved: ACTEMP (codechef)

---
+++ codechef/Practice/ACTEMP.c
@@ -0,0 +1,28 @@
+// Problem: ACTEMP
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/ACTEMP
+// Solved on: 2026-09-06T18:32:13.745Z
+
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++) {
+        int a, b, c;
+        scanf("%d %d %d", &a, &b, &c);
+
+        if (a <= b && c <= b) 
+        printf("Yes\n");
+        else {
+        printf("No\n");
+        
+            
+        }
+    }
+
+    return 0;
+}
