1ce291892e61507344fe9b48f9cc83058a1afebe
aamir27bit <aamirbharmal70@gmail.com>
2026-09-07 23:26:04 +0530

    Solved: SST (codechef)

---
+++ codechef/Practice/SST.c
@@ -0,0 +1,32 @@
+// Problem: SST
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/SST
+// Solved on: 2026-09-07T17:56:05.051Z
+
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+
+    for (int i = 0; i < T; i++) {
+        
+        int A, B;
+        
+        scanf("%d %d", &A, &B);
+
+        if (2 * A > B) 
+        printf("FIRST\n");
+        
+        else if (2 * A == B) 
+        printf("ANY\n");
+        
+        else 
+        printf("SECOND\n");
+        
+    }
+
+    return 0;
+}
