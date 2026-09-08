f48721db8dff34254457cacd816a22266d4e1285
aamir27bit <aamirbharmal70@gmail.com>
2026-09-08 20:05:49 +0530

    Solved: QUALIFY (codechef)

--- codechef/Practice/QUALIFY.c
+++ codechef/Practice/QUALIFY.c
@@ -3,24 +3,9 @@
 // Language: C​
 // Verdict: Accepted
 // URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/QUALIFY
-// Solved on: 2026-09-08T12:27:36.308Z
+// Solved on: 2026-09-08T14:35:49.698Z
 
-#include <stdio.h>
-
-int main() {
-    int T;
-    scanf("%d", &T);
-
-    for (int i = 0; i < T; i++) {
-        int X, A, B;
-        scanf("%d %d %d", &X, &A, &B);
-
-        if (A + 2 * B >= X) 
-        printf("Qualify\n");
-        else
-        printf("NotQualify\n");
-        }
-    }
-
-    return 0;
-}
+3
+15 9 3
+5 3 0
+6 2 8
