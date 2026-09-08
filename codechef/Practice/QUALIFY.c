d002af2832901664f4dcdf24a33dbc3e97b5be24
aamir27bit <aamirbharmal70@gmail.com>
2026-09-08 17:57:36 +0530

    Solved: QUALIFY (codechef)

---
+++ codechef/Practice/QUALIFY.c
@@ -0,0 +1,11 @@
+// Problem: QUALIFY
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/QUALIFY
+// Solved on: 2026-09-08T12:27:36.308Z
+
+3
+15 9 3
+5 3 0
+6 2 8
