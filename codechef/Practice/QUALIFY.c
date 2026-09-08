43f4e0081791925130ef88af440da80cbce8e9b3
aamir27bit <aamirbharmal70@gmail.com>
2026-09-08 18:01:48 +0530

    Update QUALIFY.c

--- codechef/Practice/QUALIFY.c
+++ codechef/Practice/QUALIFY.c
@@ -5,7 +5,22 @@
 // URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/QUALIFY
 // Solved on: 2026-09-08T12:27:36.308Z
 
-3
-15 9 3
-5 3 0
-6 2 8
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+
+    for (int i = 0; i < T; i++) {
+        int X, A, B;
+        scanf("%d %d %d", &X, &A, &B);
+
+        if (A + 2 * B >= X) 
+        printf("Qualify\n");
+        else
+        printf("NotQualify\n");
+        }
+    }
+
+    return 0;
+}
