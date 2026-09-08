4b1229c417430afe16501071fcbccedc9724287a
aamir27bit <aamirbharmal70@gmail.com>
2026-09-08 19:00:29 +0530

    Solved: ELECTN (codechef)

---
+++ codechef/Practice/ELECTN.c
@@ -0,0 +1,37 @@
+// Problem: ELECTN
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/ELECTN
+// Solved on: 2026-09-08T13:30:29.234Z
+
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+
+    for (int i = 0; i < T; i++) {
+        
+        int N, X;
+        
+        scanf("%d %d", &N, &X);
+
+        int count = 0;
+
+        for (int j = 0; j < N; j++) {
+            
+            int age;
+            
+            scanf("%d", &age);
+
+            if (age >= X) {
+            count++;
+            }
+        }
+
+        printf("%d\n", count);
+    }
+
+    return 0;
+}
