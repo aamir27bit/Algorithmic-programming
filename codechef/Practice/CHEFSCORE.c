614611969f87d6e79467ef4730618c2d77b26469
aamir27bit <aamirbharmal70@gmail.com>
2026-09-10 23:17:57 +0530

    Solved: CHEFSCORE (codechef)

---
+++ codechef/Practice/CHEFSCORE.c
@@ -0,0 +1,31 @@
+// Problem: CHEFSCORE
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/CHEFSCORE
+// Solved on: 2026-09-10T17:47:56.601Z
+
+#include <stdio.h>
+
+int main()
+{
+    int t;
+
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++)
+    {
+        int n, x, y;
+
+        scanf("%d %d %d", &n, &x, &y);
+
+        if (y <= n * x && y % x == 0)
+        printf("YES\n");
+        
+        else
+        printf("NO\n");
+        
+    }
+
+    return 0;
+}
