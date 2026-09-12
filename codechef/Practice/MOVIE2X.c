2aff26bcabd44fc2a9583544b3370aeca3982378
aamir27bit <aamirbharmal70@gmail.com>
2026-09-12 21:59:42 +0530

    Solved: MOVIE2X (codechef)

---
+++ codechef/Practice/MOVIE2X.c
@@ -0,0 +1,23 @@
+// Problem: MOVIE2X
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/MOVIE2X
+// Solved on: 2026-09-12T16:29:44.082Z
+
+#include <stdio.h>
+
+int main()
+{
+    int x, y, z;
+
+    scanf("%d %d", &x, &y);
+
+    z = y / 2;
+
+    z = z + (x - y);
+
+    printf("%d\n", z);
+
+    return 0;
+}
