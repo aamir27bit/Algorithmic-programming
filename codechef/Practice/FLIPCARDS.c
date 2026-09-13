c8ec14a6dcbd57009a8333058e2932fc66e34866
aamir27bit <aamirbharmal70@gmail.com>
2026-09-13 22:46:33 +0530

    Solved: FLIPCARDS (codechef)

---
+++ codechef/Practice/FLIPCARDS.c
@@ -0,0 +1,29 @@
+// Problem: FLIPCARDS
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/FLIPCARDS
+// Solved on: 2026-09-13T17:16:32.276Z
+
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++) {
+        
+        int x, y;
+        
+        scanf("%d %d", &x, &y);
+
+        if (x>0 && y>0 && x-y>0) 
+        printf("%d\n", y);
+        
+        else
+        printf("0\n");
+        
+    }
+
+    return 0;
+}
