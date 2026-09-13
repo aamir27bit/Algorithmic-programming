afe3c251927272317e8e2d9b5446ee0f2d68eeaa
aamir27bit <aamirbharmal70@gmail.com>
2026-09-13 23:59:23 +0530

    Solved: FINDSHOES (codechef)

---
+++ codechef/Practice/FINDSHOES.c
@@ -0,0 +1,29 @@
+// Problem: FINDSHOES
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/FINDSHOES
+// Solved on: 2026-09-13T18:29:23.306Z
+
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++) {
+        
+        int x, y;
+        
+        scanf("%d %d", &x, &y);
+
+        if (y >= x)
+        printf("%d\n", x);
+        
+        else 
+        printf("%d\n", 2 * x - y);
+        
+    }
+
+    return 0;
+}
