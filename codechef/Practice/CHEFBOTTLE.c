56d25557d042e890088e0d351575685ad11068d1
aamir27bit <aamirbharmal70@gmail.com>
2026-09-17 23:37:58 +0530

    Solved: CHEFBOTTLE (codechef)

---
+++ codechef/Practice/CHEFBOTTLE.c
@@ -0,0 +1,30 @@
+// Problem: CHEFBOTTLE
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/CHEFBOTTLE
+// Solved on: 2026-09-17T18:07:58.242Z
+
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++) {
+        
+        int n, x, k;
+        
+        scanf("%d %d %d", &n, &x, &k);
+
+        int bottles = k / x;
+
+        if (bottles > n) {
+            bottles = n;
+        }
+
+        printf("%d\n", bottles);
+    }
+
+    return 0;
+}
