f987d43c1450a8568eca8558e269a53f1910e477
aamir27bit <aamirbharmal70@gmail.com>
2026-09-18 22:09:23 +0530

    Solved: FSQRT (codechef)

---
+++ codechef/Practice/FSQRT.c
@@ -0,0 +1,26 @@
+// Problem: FSQRT
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/FSQRT
+// Solved on: 2026-09-18T16:39:23.509Z
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++) {
+        
+        int n;
+        
+        scanf("%d", &n);
+
+        printf("%d\n", (int)sqrt(n));
+    }
+
+    return 0;
+}
