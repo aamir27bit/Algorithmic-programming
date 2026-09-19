0d9b8938212f28afe6671b5921e6d34d4d8f7b38
aamir27bit <aamirbharmal70@gmail.com>
2026-09-19 21:27:46 +0530

    Solved: LASTLEVELS (codechef)

---
+++ codechef/Practice/LASTLEVELS.c
@@ -0,0 +1,26 @@
+// Problem: LASTLEVELS
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/LASTLEVELS
+// Solved on: 2026-09-19T15:57:46.976Z
+
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    while (t--) {
+        
+        int x, y, z;
+        
+        scanf("%d %d %d", &x, &y, &z);
+
+        int breaks = (x - 1) / 3;
+
+        printf("%d\n", x * y + breaks * z);
+    }
+
+    return 0;
+}
