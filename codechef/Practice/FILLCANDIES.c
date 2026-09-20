4dfd73b358bdaaf32ef773dc5820ec779de66a7c
aamir27bit <aamirbharmal70@gmail.com>
2026-09-20 19:20:46 +0530

    Solved: FILLCANDIES (codechef)

---
+++ codechef/Practice/FILLCANDIES.c
@@ -0,0 +1,28 @@
+// Problem: FILLCANDIES
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/FILLCANDIES
+// Solved on: 2026-09-20T13:50:46.909Z
+
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++) {
+        
+        int x, y, z;
+        
+        scanf("%d %d %d", &x, &y, &z);
+
+        if (y * z >= x)
+        printf("1\n");
+        
+        else
+        printf("%d\n", (x + y * z - 1) / (y * z));
+    }
+
+    return 0;
+}
