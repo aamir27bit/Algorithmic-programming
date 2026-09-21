6fb631689d881e3b735855dc33bba9639358e549
aamir27bit <aamirbharmal70@gmail.com>
2026-09-21 22:58:26 +0530

    Solved: XJUMP (codechef)

---
+++ codechef/Practice/XJUMP.c
@@ -0,0 +1,24 @@
+// Problem: XJUMP
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/XJUMP
+// Solved on: 2026-09-21T17:28:25.750Z
+
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++) {
+        
+        int x, y;
+        
+        scanf("%d %d", &x, &y);
+
+        printf("%d\n", x / y + x % y);
+    }
+
+    return 0;
+}
