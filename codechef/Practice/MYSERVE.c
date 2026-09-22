265a85121a76801a94b57c645f6ba0c8fb208473
aamir27bit <aamirbharmal70@gmail.com>
2026-09-22 22:24:03 +0530

    Solved: MYSERVE (codechef)

---
+++ codechef/Practice/MYSERVE.c
@@ -0,0 +1,26 @@
+// Problem: MYSERVE
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/MYSERVE
+// Solved on: 2026-09-22T16:54:03.125Z
+
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++) {
+        int p, q;
+        scanf("%d %d", &p, &q);
+
+        if ((p + q) % 4 < 2)
+        printf("Alice\n");
+        
+        else
+        printf("Bob\n");
+    }
+
+    return 0;
+}
