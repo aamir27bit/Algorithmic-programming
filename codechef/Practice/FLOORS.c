bbc5bfc4073ef468d53f7d93c40f17d7be15afc2
aamir27bit <aamirbharmal70@gmail.com>
2026-09-23 16:59:22 +0530

    Solved: FLOORS (codechef)

---
+++ codechef/Practice/FLOORS.c
@@ -0,0 +1,31 @@
+// Problem: FLOORS
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/FLOORS
+// Solved on: 2026-09-23T11:29:21.935Z
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++) {
+
+        int x, y, floorX, floorY, z;
+
+        scanf("%d %d", &x, &y);
+
+        floorX = (x + 9) / 10;
+        floorY = (y + 9) / 10;
+
+        z = abs(floorX - floorY);
+
+        printf("%d\n", z);
+    }
+
+    return 0;
+}
