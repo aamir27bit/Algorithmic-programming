515bc93f8dd209d7be7adfd6369961a0e1f9096c
aamir27bit <aamirbharmal70@gmail.com>
2026-09-23 15:30:00 +0530

    Solved: CHEFEREN (codechef)

---
+++ codechef/Practice/CHEFEREN.c
@@ -0,0 +1,29 @@
+// Problem: CHEFEREN
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/CHEFEREN
+// Solved on: 2026-09-23T10:00:00.313Z
+
+#include <stdio.h>
+
+int main() {
+
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++) {
+
+        int n, a, b;
+        scanf("%d %d %d", &n, &a, &b);
+
+        int odd = (n + 1) / 2;
+        int even = n / 2;
+
+        int total = odd * b + even * a;
+
+        printf("%d\n", total);
+    }
+
+    return 0;
+}
