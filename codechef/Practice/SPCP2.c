0f0a7ec0cc8d2b663f2b51d5e08a6b486bf0234a
aamir27bit <aamirbharmal70@gmail.com>
2026-09-23 16:10:47 +0530

    Solved: SPCP2 (codechef)

---
+++ codechef/Practice/SPCP2.c
@@ -0,0 +1,28 @@
+// Problem: SPCP2
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/SPCP2
+// Solved on: 2026-09-23T10:40:47.157Z
+
+#include <stdio.h>
+
+int main() {
+
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++) {
+
+        int x, n;
+        scanf("%d %d", &x, &n);
+
+        if (x * 100 >= n)
+        printf("0\n");
+        
+        else
+        printf("%d\n", (n + 99) / 100 - x);
+    }
+
+    return 0;
+}
