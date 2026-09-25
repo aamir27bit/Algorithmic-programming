09d48af10443e36dc89b17d657bb1aaa435016c9
aamir27bit <aamirbharmal70@gmail.com>
2026-09-25 20:08:20 +0530

    Solved: LCPPAS01 (codechef)

---
+++ codechef/Practice/LCPPAS01.cpp
@@ -0,0 +1,14 @@
+// Problem: LCPPAS01
+// Platform: codechef
+// Language: #include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << 21 + 40;
+    return 0;
+}
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/cpp/LPCPAS01/problems/LCPPAS01
+// Solved on: 2026-09-25T14:38:20.354Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
