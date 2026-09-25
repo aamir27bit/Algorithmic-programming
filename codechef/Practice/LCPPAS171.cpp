d4316f3bec62c3a2b274bd585de412d1f3d51b34
aamir27bit <aamirbharmal70@gmail.com>
2026-09-25 23:07:15 +0530

    Solved: LCPPAS171 (codechef)

---
+++ codechef/Practice/LCPPAS171.cpp
@@ -0,0 +1,30 @@
+// Problem: LCPPAS171
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/cpp/LPCPAS06/problems/LCPPAS171
+// Solved on: 2026-09-25T17:37:14.986Z
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    if (a == b && b == c) 
+    cout << "Equilateral";
+    
+
+    else if (a != b && b != c && a != c) 
+    cout << "Scalene";
+    
+
+    else 
+    cout << "Isosceles";
+    
+
+    return 0;
+}
