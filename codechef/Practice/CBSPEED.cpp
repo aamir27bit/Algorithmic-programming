47fc3bd37c25db45b53aed22529249ebb1c9ff7a
aamir27bit <aamirbharmal70@gmail.com>
2026-09-25 23:33:30 +0530

    Solved: CBSPEED (codechef)

---
+++ codechef/Practice/CBSPEED.cpp
@@ -0,0 +1,24 @@
+// Problem: CBSPEED
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/cpp/PCPP03/problems/CBSPEED
+// Solved on: 2026-09-25T18:03:29.169Z
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+
+    cin >> x >> y;
+
+    if (y > x) 
+    cout << "YES";
+    
+    else 
+    cout << "NO";
+    
+
+    return 0;
+}
