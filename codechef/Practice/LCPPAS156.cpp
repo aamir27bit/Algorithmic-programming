de379c0120727020cd7ea1035d2e2e3fc8149e3c
aamir27bit <aamirbharmal70@gmail.com>
2026-09-25 22:47:01 +0530

    Solved: LCPPAS156 (codechef)

---
+++ codechef/Practice/LCPPAS156.cpp
@@ -0,0 +1,29 @@
+// Problem: LCPPAS156
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/cpp/LPCPAS06/problems/LCPPAS156
+// Solved on: 2026-09-25T17:17:00.684Z
+
+#include <iostream>
+using namespace std;
+#include<string>
+
+
+int main() {
+
+    string s1;
+    string s2;
+
+  cin>>s1>> s2;
+  
+  int x,y;
+  cin>>x>> y;
+  
+  if(x>y)
+  cout<< s1;
+  
+  else
+  cout<< s2;
+  
+}
