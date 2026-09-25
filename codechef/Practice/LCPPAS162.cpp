c9c8de05f2e2b55cc0cff271b611ad91e17a39ba
aamir27bit <aamirbharmal70@gmail.com>
2026-09-26 00:42:02 +0530

    Solved: LCPPAS162 (codechef)

---
+++ codechef/Practice/LCPPAS162.cpp
@@ -0,0 +1,30 @@
+// Problem: LCPPAS162
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/cpp/LPCPAS11/problems/LCPPAS162
+// Solved on: 2026-09-25T19:12:02.264Z
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int sum = 0;
+    int product = 1;
+
+    while (n > 0) {
+        int digit = n % 10;
+
+        sum = sum + digit;
+        product = product * digit;
+
+        n = n / 10;
+    }
+
+    cout << sum << " " << product;
+
+    return 0;
+}
